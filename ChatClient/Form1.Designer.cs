52b718aea7c8b618a509155ecb93fd75dde12c99
ozgunesim <ozgunesim@gmail.com>
2017-07-30 15:11:21 +0300

    first commit

---
+++ ChatClient/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGelen = new System.Windows.Forms.TextBox();
+            this.txtGiden = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtGelen
+            // 
+            this.txtGelen.Location = new System.Drawing.Point(0, 54);
+            this.txtGelen.Multiline = true;
+            this.txtGelen.Name = "txtGelen";
+            this.txtGelen.Size = new System.Drawing.Size(523, 301);
+            this.txtGelen.TabIndex = 0;
+            // 
+            // txtGiden
+            // 
+            this.txtGiden.Location = new System.Drawing.Point(0, 361);
+            this.txtGiden.Name = "txtGiden";
+            this.txtGiden.Size = new System.Drawing.Size(442, 20);
+            this.txtGiden.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(448, 359);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Gönder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtIP
+            // 
+            this.txtIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtIP.Location = new System.Drawing.Point(57, 6);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(188, 32);
+            this.txtIP.TabIndex = 3;
+            this.txtIP.Text = "127.0.0.1";
+            this.txtIP.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP:";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPort.Location = new System.Drawing.Point(251, 9);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(78, 26);
+            this.lblPort.TabIndex = 5;
+            this.lblPort.Text = "PORT:";
+            // 
+            // txtPort
+            // 
+            this.txtPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtPort.Location = new System.Drawing.Point(335, 6);
+            this.txtPort.Name = "txtPort";
+            this.txtPort.Size = new System.Drawing.Size(188, 32);
+            this.txtPort.TabIndex = 6;
+            this.txtPort.Text = "10000";
+            this.txtPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(531, 386);
+            this.Controls.Add(this.txtPort);
+            this.Controls.Add(this.lblPort);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtGiden);
+            this.Controls.Add(this.txtGelen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGelen;
+        private System.Windows.Forms.TextBox txtGiden;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.TextBox txtPort;
+    }
+}
+
---
+++ ChatClient/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TcpClient gonder = new TcpClient(txtIP.Text, Convert.ToInt32(txtPort.Text));
+            NetworkStream okuryazar = gonder.GetStream();
+            string mesaj = txtGiden.Text;
+            byte[] mesaj_dizi = Encoding.ASCII.GetBytes(mesaj);
+            byte[] gelen = new byte[gonder.ReceiveBufferSize];
+
+            if (okuryazar.CanWrite)
+            {
+                okuryazar.Write(mesaj_dizi, 0, mesaj_dizi.Length);
+            }
+            else
+            {
+                gonder.Close();
+                return;
+            }
+            if (okuryazar.CanRead)
+            {
+                okuryazar.Read(gelen, 0, (int)gonder.ReceiveBufferSize);
+                string gelenveri = Encoding.ASCII.GetString(gelen);
+                txtGelen.Text = Environment.NewLine + gelenveri;
+            }
+            else
+            {
+                gonder.Close();
+                return;
+            }
+            
+            txtGiden.Text = "";
+            mesaj = "";
+        }
+    }
+}
